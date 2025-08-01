8d46b1aad98567386bf69350469cc0b2ded73002
CristianV22 <cristian.v.s.oliveira@gmail.com>
2025-08-01 15:49:05 -0300

    .

--- petlov-template/cypress.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  projectId: "6qqug8",
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    baseUrl: 'https://petlov.vercel.app'
-  },
-};
